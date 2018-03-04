7dbc737c983bbb0eec1ed68a77beb0e57854d80e
Bulava Eduard <bulavaeduard@gmail.com>
2018-03-04 16:27:20 +0200

    fix issue wit building app

--- android/app/src/main/java/com/chatapp/MainActivity.java
+++ android/app/src/main/java/com/chatapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "h217-client";
+        return "ChatApp";
     }
 }
