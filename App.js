abf0ca7eecc727991f795d7d18dce1c6f02f5d56
Bulava Eduard <bulavaeduard@gmail.com>
2018-03-04 12:53:17 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+
+      return (
+      <View style={styles.container}>
+          <Button/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
