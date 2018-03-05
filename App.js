5ca1914fc341013817bd30fc29bed9808b1238ac
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2018-03-05 21:20:50 +0200

    First Attempt to create facebook registration

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
-import Chat from "./components/Chat";
+import { View, TouchableOpacity } from 'react-native';
+// import Chat from "./components/Chat";
+import FBSDK, { LoginManager } from 'react-native-fbsdk';
 
 export default class App extends React.Component {
-  render() {
-      return (
-      <View>
-          <Chat/>
-      </View>
-    );
-  }
+    _fbAuth() {
+        LoginManager.logInWithReadPermissions(['public_profile']).then(function (result) {
+            if (result.isCancelled) {
+                console.log('Login was cancelled')
+            } else {
+                console.log('Login was success ' + result.grantedPermissions.toString())
+            }
+        }, function (error) {
+            console.log('Something went wrong' + error)
+        })
+    }
+    render() {
+        return (
+            <View>
+                <TouchableOpacity onPress={this._fbAuth()}>
+                    <Text>Test</Text>
+                </TouchableOpacity>
+                {/*<Chat/>*/}
+            </View>
+        );
+    }
 }
 
